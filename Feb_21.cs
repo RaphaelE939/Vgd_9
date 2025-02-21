182323045f459b66ebc642b42a640d53af0b8171
RaphaelE939 <raphaelE939@stemcollegiate.ca>
2025-02-21 08:55:09 -0700

    Update Feb_21.cs

--- Feb_21.cs
+++ Feb_21.cs
@@ -1,1 +1,7 @@
-""//, is used to identify a string 
+""//, is used to identify a string
+  string firstName = "Raphael";
+int number = 67;
+double temp = -32.2;
+Console.WriteLine($"Hello, {firstName}! You have {number} messages in your inbox. Temperature is {temp} celsius.");//:
+// is used to write the message; Hello, Raphael! You have 67 messages in your inbox. Temperature is -32.2 celsius.
+// Declaring variables is very important.
