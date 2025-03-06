d11a69b893720038d9b00b816bfe87ffdaa7bd2c
RaphaelE939 <raphaelE939@stemcollegiate.ca>
2025-03-06 10:11:07 -0700

    Update Feb_18.cs

--- Feb_18.cs
+++ Feb_18.cs
@@ -1,5 +1,5 @@
 // I learned how to declare a variables like the char variable, int variable, and decimal, and bool variable
-// char userOption;
-// int gameScore;
-// decimal particlesPerMillion;
-// bool processedCustomer;
+char userOption;
+int gameScore;
+decimal particlesPerMillion;
+bool processedCustomer;
